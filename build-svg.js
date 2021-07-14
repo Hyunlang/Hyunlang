186bca7e41638d570f383880c51b2b4c724e925e
Hyunlang <74277485+Hyunlang@users.noreply.github.com>
2021-07-14 16:22:36 +0900

    Create build-svg.js

---
+++ build-svg.js
@@ -0,0 +1,16 @@
+let fs = require("fs");
+
+fs.readFile("template.svg", "utf-8", (error, data) => {
+  if (error) {
+    return;
+  }
+  
+  data = data.replace('{name}', 'uiandwe')
+
+  data = fs.writeFile("chat.svg", data, (err) => {
+    if (err) {
+      console.error(err);
+      return;
+    }
+  });
+});
